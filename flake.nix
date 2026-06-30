6748fc3ecc7fad40484b20488b2f6ed696fc8d67
woker <wokerNM@proton.me>
2026-06-29 21:41:17 -0700

    Update flake.lock & flake.nix

--- flake.nix
+++ flake.nix
@@ -3,66 +3,58 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    neko = {
+      url = "github:the-woker/neko-mangowm";
+      flake = false;
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
+      neko,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "neko-mangowm";
+        version = "0.1.0";
 
-        neko-mangowm = pkgs.rustPlatform.buildRustPackage {
-          pname = "neko-mangowm";
-          version = "0.1.0";
+        src = neko;
+        sourceRoot = "source/mangowm";
 
-          src = self.inputs.neko or pkgs.fetchFromGitHub {
-            owner = "the-woker";
-            repo = "neko-mangowm";
-            rev = "1b3750d0697498c29c55b31b812b8ed71774d35c";
-            hash = "sha256-3BWuBGDsvlyeio7ZyviSfiPF3pwmntBfs8k9eIzucuo=";
-          };
-
-          cargoLock = {
-            lockFile = "${neko-mangowm.src}/Cargo.lock";
-          };
-
-          cargoBuildFlags = [
-            "-p"
-            "mangowm"
-          ];
-
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-
-          buildInputs = with pkgs; [
-            libx11
-            libxrandr
-            libxinerama
-            libxkbcommon
-          ];
-        };
-      in
-      {
-        packages.default = neko-mangowm;
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = neko-mangowm;
+        cargoLock = {
+          lockFile = "${neko}/Cargo.lock";
         };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ neko-mangowm ];
-          nativeBuildInputs = with pkgs; [
-            cargo
-            rustc
-          ];
-        };
-      }
-    );
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          libx11
+          libxrandr
+          libxinerama
+          libxkbcommon
+        ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          cargo
+          rustc
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          libx11
+          libxrandr
+          libxinerama
+          libxkbcommon
+        ];
+      };
+    };
 }
